a716ba48999fb346d5afb3b513f46d8af20a0c6d
fmb <contactfmb@gmail.com>
2013-07-03 12:05:49 -0400

    Ticket WLPROD-67 Completed

--- WLPROD-61/js/untitled folder/main.js
+++ WLPROD-61/js/untitled folder/main.js
@@ -5,33 +5,31 @@ $(document).ready(function() {
 	//scoping the function, each wl-item is to be treated differently
 	$('.wl-item').each(function (i) { 
 
-
-			// set $this as variable to avoid jumping back into DOM
-			var $this = $(this),
 			// setting mainimage equal 
-			mainimage = $this.find( $('.wl-thumbnail') );
+			var mainimage = $(this).find( $('.wl-thumbnail') );
+
 			//defining what a swatch is, equal to IMG elements under a class of .swatches
-			$swatch = $this.find('.swatches img');
+			$swatch = $(this).find('.swatches img');
+			
 			//on-click event for clicking on a swatch
 			$swatch.on('click', function() {			
+
   				//src = this attribute(src) value of x
-  				var src = $(this).data('src'),
+  				var src = $(this).attr("data-src");
+
   				//mainimage_source = "src" to be changed
-  				mainimage_source = mainimage.attr("src");
+  				var mainimage_source = mainimage.attr("src")
+
   				// if the mainimage_source is not equal to the source of the clicked swatch change it
   				if (mainimage_source != src ) {
   				
   				//passing attr function src attribute and SRC variable
 				mainimage.attr('src', src); 
-				console.log('not equal, change it');
 				
 				//do nuthin'
 				} else {
 
-					console.log('equal, dont change anything');
-					
 					return;
-				
 				}
 
 			});
@@ -39,3 +37,12 @@ $(document).ready(function() {
 	});		
 
 })
+
+
+// $(document).ready(function() {
+// 	alert("Hello!");
+// });
+
+
+
+
