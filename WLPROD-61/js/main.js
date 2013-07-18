3734dad2b1d353e8239b02ce0892793e134e1cc0
fmb <contactfmb@gmail.com>
2013-07-18 09:47:53 -0400

    WLPROD-61/

--- WLPROD-61/js/main.js
+++ WLPROD-61/js/main.js
@@ -1,41 +1,60 @@
 
 // wait for dom to load
 $(document).ready(function() {
-
 	//scoping the function, each wl-item is to be treated differently
 	$('.wl-item').each(function (i) { 
-
-
-			// set $this as variable to avoid jumping back into DOM
-			var $this = $(this),
-			// setting mainimage equal 
-			mainimage = $this.find( $('.wl-thumbnail') );
-			//defining what a swatch is, equal to IMG elements under a class of .swatches
-			$swatch = $this.find('.swatch_list a');
-			//on-click event for clicking on a swatch
-			$swatch.on('click', function() {			
-  				//src = this attribute(src) value of x
-  				var src = $(this).data('src'),
-  				//mainimage_source = "src" to be changed
-  				mainimage_source = mainimage.attr("src");
-  				// if the mainimage_source is not equal to the source of the clicked swatch change it
-  				if (mainimage_source != src ) {
-  				
+		// set $this as variable to avoid jumping back into DOM
+		var $this = $(this),
+		// setting mainimage equal 
+		mainimage = $this.find( $('.wl-thumbnail') );
+		//defining what a swatch is, equal to IMG elements under a class of .swatches
+		$swatch = $this.find('.swatch_list a');
+		//on-click event for clicking on a swatch
+		$swatch.on('click', function() {			
+  			//src = this attribute(src) value of x
+  			var src = $(this).data('src'),
+  			//mainimage_source = "src" to be changed
+  			mainimage_source = mainimage.attr("src");
+  			// if the mainimage_source is not equal to the source of the clicked swatch change it
+  			if (mainimage_source != src ) {
   				//passing attr function src attribute and SRC variable
 				mainimage.attr('src', src); 
 				// console.log('not equal, change it');
-				
-				//do nuthin'
 				} else {
-
-					// console.log('equal, dont change anything');
-					
-					return;
-				
-				}
-
-			});
-
+				// console.log('equal, dont change anything');				
+				return;							
+			}
+		});
 	});		
+});
 
+
+// Wait for DOM to load
+$(document).ready(function(){
+	//iterate only through .wl-navigation and it's children
+	$('.wl-navigation').each(function() {
+		//set $this equal for re-use to avoid jumping back into DOM
+		var $this = $(this),
+			//set SubList variable = to any element with a class .sub-menu
+			$subLists = $this.find('.sub-menu'),
+			//set Anchors = to any sibling occuring 1 step above the sublist
+			$anchors = $subLists.prev('.list-item');
+		//when you click the anchor run this function
+		$anchors.on('click', function(e){
+			//nullify default on-click behavior to avoid jumping to top of page
+			e.preventDefault();
+			//scope this to this anchor
+			var $thisAnchor = $(this),
+			//scope target to this anchor
+				$thisSubLists = $thisAnchor.find('+ .sub-menu');
+			//remove is-inactive from UL
+			$thisSubLists.toggleClass('is-inactive');
+			//add is-active to anchor tag
+			$thisAnchor.toggleClass('is-active');
+		});
+	});
 })
+
+
+
+
